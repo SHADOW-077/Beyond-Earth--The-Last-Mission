3959adfcb2b33c165d144c6d353bf64e66ef7c08
shadow <144371189+SHADOW-077@users.noreply.github.com>
2024-08-10 21:01:19 +0530

    updated touch screen controls

--- scenes/rover.gd
+++ scenes/rover.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var rover_speed = 250
+@export var rover_speed = 325
 @export var acceleration = 5
 var movement = 0
 @export var rotation_speed = 1.75
@@ -14,10 +14,10 @@ func _physics_process(delta):
 	var direction_x = Input.get_axis("move_left","move_right")
 	if direction_x !=0 && movement !=0:
 		rotation += .6 *direction_x *delta
-	if Input.is_action_pressed("move_backward"):
+	if Input.is_action_pressed("move_forward"):
+			movement+= acceleration
+			velocity = lerp(velocity,Vector2(min(movement, rover_speed),0).rotated(rotation),0.5)
+	elif Input.is_action_pressed("move_backward"):
 		movement-= acceleration
 		velocity = lerp(velocity,Vector2(max(movement, -rover_speed/2),0).rotated(rotation),0.5)
-	elif Input.is_action_pressed("move_forward"):
-		movement+= acceleration
-		velocity = lerp(velocity,Vector2(min(movement, rover_speed),0).rotated(rotation),0.5)
 	move_and_slide()
